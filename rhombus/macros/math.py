53860b90cc4d2b319f8a64c453fe5454f4d56030
Annhilati <78727320+annhilati@users.noreply.github.com>
2026-01-10 19:05:30 +0100

    Added a lot of math functions

--- rhombus/macros/math.py
+++ rhombus/macros/math.py
@@ -7,7 +7,12 @@ from rhombus.language import builtins as f
 from typing import Callable
 
 pi = 3.14159265359
+"The constant π to 11 decimals."
 e = 2.71828182846
+"Euler's number e to 10 decimals."
+
+
+#======// Number Theory //=======================================================================//
 
 def heaviside(argument: DensityDescriptor):
     """Returns the Heaviside function value of the input which is `0.0` when the input is negative and `1.0` when it is positive.<br>
@@ -16,10 +21,7 @@ def heaviside(argument: DensityDescriptor):
     arg, = resolve_shorthands(argument)
     return 0.5 * (sgn(arg) + 1)
 
-def monus(argument1: DensityDescriptor, argument2: DensityDescriptor):
-    """Returns `argument1 - argument2`, but when that's negative, returns `0.0` instead."""
-    arg1, arg2 = resolve_shorthands(argument1, argument2)
-    return f.max(arg1 - arg2, 0.0)
+"https://en.wikipedia.org/wiki/Stirling%27s_approximation"
 
 def ramp(argument: DensityDescriptor):
     """Returns the ramp function value of the input, meaning `argument1` itself, when it's positive, otherwise returns `0.0`."""
@@ -80,23 +82,84 @@ def sqrt(argument: DensityDescriptor, iterations: int = 3, guess: Callable[[Dens
 
     return x
 
+def exp(argument: DensityDescriptor, terms: int = 4) -> Density[dft.add]:
+    """Returns `e` exponentiated to the input.<br>
+
+    ⚙️ This implementation uses the [Taylor series of the exponential function](https://en.wikipedia.org/wiki/Taylor_series#Exponential_function).<br>
+    """
+    from math import factorial
+
+    arg, = resolve_shorthands(argument)
+
+    result = f.constant(1)
+    for k in range(1, terms + 1):
+        term = (arg ** k) / factorial(k)
+        result += term
+
+    return result
+
+def ln(argument: DensityDescriptor, terms: int = 4) -> Density[dft.add]:
+    """Returns the natual logarithm value of the input.<br>
+
+    ⚙️ This implementation uses the [Taylor series of the natural logarithm](https://en.wikipedia.org/wiki/Taylor_series#Natural_logarithm).<br>
+    """
+    arg, = resolve_shorthands(argument)
+
+    y = arg - f.constant(1)
+
+    result = f.constant(0)
+    for k in range(1, terms + 1):
+        coeff = -1 if (k % 2 == 0) else 1
+        term = coeff * (y ** k) / k
+        result += term
+
+    return result
+
+#======// Arithmetic //==========================================================================//
+
+def monus(argument1: DensityDescriptor, argument2: DensityDescriptor):
+    """Returns `argument1 - argument2`, but when that's negative, returns `0.0` instead."""
+    arg1, arg2 = resolve_shorthands(argument1, argument2)
+    return f.max(arg1 - arg2, 0.0)
+
+def sum(*arguments: DensityDescriptor) -> Density[dft.add]:
+    "Returns the sum of any number of arguments."
+    if len(arguments) == 1:
+        return arguments[0]
+    
+    arguments = resolve_shorthands(*arguments)
+
+    it = iter(arguments)
+    result = next(it) + next(it)
+
+    for x in it:
+        result = result + x
+
+    return result
+
+def prod(*arguments: DensityDescriptor) -> Density[dft.mul]:
+    "Returns the product of any number of arguments."
+    if len(arguments) == 1:
+        return arguments[0]
+    
+    arguments = resolve_shorthands(*arguments)
+
+    it = iter(arguments)
+    result = next(it) * next(it)
+
+    for x in it:
+        result = result * x
+
+    return result
+
+
+#======// Trigonometry //========================================================================//
+
 def sin(argument: DensityDescriptor, terms: int = 4) -> Density[dft.add]:
     """Returns the sine value of the input in radians.<br>
 
     ⚙️ This implementation uses the [Taylor series of sine](https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions).<br>
     ⚠️ Bigger inputs need more terms before converging.
-
-    #### Precision for inputs between `1` and `-1`
-    ```
-    Terms  │ Decimals │ Calculations
-    ═══════╪══════════╪══════════════════════════════
-    1      │ 0        │
-    2      │ 2        │ 
-    3      │ 3        │ 
-    4      │ 5        │ 
-    5      │ 7        │ 
-    ```
-    
     """
     from math import factorial
 
@@ -116,17 +179,6 @@ def cos(argument: DensityDescriptor, terms: int = 4) -> Density[dft.add]:
 
     ⚙️ This implementation uses the [Taylor series of cosine](https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions).<br>
     ⚠️ Bigger inputs need more terms before converging.
-
-    #### Precision for inputs between `1` and `-1`
-    ```
-    Terms  │ Decimals │ Calculations
-    ═══════╪══════════╪══════════════════════════════
-    1      │ 0        │
-    2      │ 2        │
-    3      │ 4        │
-    4      │ 6        │
-    5      │ 8        │
-    ```
     """
     from math import factorial
 
@@ -151,3 +203,79 @@ def tan(argument: DensityDescriptor, terms: int = 4) -> Density[dft.mul]:
     arg, = resolve_shorthands(argument)
 
     return sin(arg, terms) / cos(arg, terms)
+
+def asin(argument: DensityDescriptor, terms: int = 4) -> Density[dft.add]:
+    """Returns the arc sine value of the input in radians.<br>
+    ❗`asin(x) = NaN`, if `x < -1` or `x > 1`
+
+    ⚙️ This implementation uses the [Taylor series of arc sine](https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions).<br>
+    """
+    from math import factorial
+
+    arg, = resolve_shorthands(argument)
+
+    result = f.constant(0)
+    for k in range(terms):
+        coeff = factorial(2*k) / (4**k * factorial(k)**2 * (2*k + 1))
+        term = coeff * (arg ** (2*k + 1))
+        result = result + term
+
+    return result
+
+def acos(argument: DensityDescriptor, terms: int = 4) -> Density[dft.add]:
+    """Returns the arc cosine value of the input in radians.<br>
+    ❗`acos(x) = NaN`, if `x < -1` or `x > 1`
+
+    ⚙️ This implementation uses the shifted [Taylor series of arc sine](https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions).<br>
+    """
+    arg, = resolve_shorthands(argument)
+    return (pi / 2) - asin(arg, terms)
+
+def atan(argument: DensityDescriptor, terms: int = 4) -> Density[dft.add]:
+    """Returns the arc tangent value of the input in radians.<br>
+
+    ⚙️ This implementation uses the [Taylor series of arc tangent](https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions).<br>
+    """
+    arg, = resolve_shorthands(argument)
+
+    result = f.constant(0)
+    for k in range(terms):
+        coeff = -1 if (k % 2) else 1
+        term = coeff * (arg ** (2*k + 1)) / (2*k + 1)
+        result = result + term
+
+    return result
+
+def sinh(argument: DensityDescriptor, terms: int = 4) -> Density[dft.add]:
+    """Returns the hyperbolic sine value of the input in radians.<br>
+
+    ⚙️ This implementation uses the [Taylor series of hyperbolic sine](https://en.wikipedia.org/wiki/Taylor_series#Hyperbolic_functions).<br>
+    """
+    from math import factorial
+
+    arg, = resolve_shorthands(argument)
+
+    result = f.constant(0)
+    for k in range(terms):
+        power = 2*k + 1
+        term = (arg ** power) / factorial(power)
+        result = result + term
+
+    return result
+
+def cosh(argument: DensityDescriptor, terms: int = 4) -> Density[dft.add]:
+    """Returns the hyperbolic cosine value of the input in radians.<br>
+
+    ⚙️ This implementation uses the [Taylor series of hyperbolic cosine](https://en.wikipedia.org/wiki/Taylor_series#Hyperbolic_functions).<br>
+    """
+    from math import factorial
+
+    arg, = resolve_shorthands(argument)
+
+    result = f.constant(1)
+    for k in range(1, terms + 1):
+        power = 2*k
+        term = (arg ** power) / factorial(power)
+        result = result + term
+
+    return result
