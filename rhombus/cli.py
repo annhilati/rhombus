93e52031a558ad5136f99e0f73f86a0e213e1d6b
Annhilati <antbe.artis@web.de>
2026-05-21 15:43:35 +0200

    CLI works for now

--- rhombus/cli.py
+++ rhombus/cli.py
@@ -4,13 +4,13 @@ from typing import Callable
 from pathlib import Path
 from pathlib import Path
 from importlib.util import spec_from_file_location, module_from_spec
-import argparse, sys, traceback, subprocess
+import argparse, sys, traceback, subprocess, shutil
 
 import beet
 from rhombus import Density
 from rich import print
 
-class CLIError(Exception): ...
+class RhombusCLIProblem(Exception): ...
 
 def resolve_path_to_module(p: Path) -> ModuleType | None:
 
@@ -26,23 +26,43 @@ def resolve_path_to_module(p: Path) -> ModuleType | None:
 
     return module
 
+def run_with_indent(cmd):
+    from builtins import print
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True
+    )
+
+    for line in process.stdout:
+        print(f"  {line}", end="")
+
+    return process.wait()
+
 def compile_project(source: Path, symbol: str = None, output_dir: Path = None, id: str = None) -> None:
     
     print("")
     print("[white on #5137d4] Rhombus Compilation [/white on #5137d4]")
     
-    if (
-        (source.is_file() and "beet" in source.name)
-        or (not source.is_file() and any(p.is_file() and p.name.startswith("beet") for p in source.iterdir()))
-        ):
-        print("Beet")
-        subprocess.run(["beet"])
+    if not source.exists():
+        raise RhombusCLIProblem(f"Path {source} does not exist")
     
+    # Beet project directory
+    if (source.is_file() and "beet" in source.name) or (not source.is_file() and any(p.is_file() and p.name.startswith("beet") for p in source.iterdir())):
+        print(f"  Detected Beet project at {source}\n")
+        run_with_indent(["beet"])
+    
+    # Python Module
     elif (module := resolve_path_to_module(source)) is not None:
-        print("Module")
-        if symbol is None: raise CLIError("Parameter symbol missing")
-        if output_dir is None: raise CLIError("Parameter out missing")
-        if id is None: raise CLIError("Parameter id missing")
+        if symbol is None: raise RhombusCLIProblem("Missing parameter: symbol (after source)")
+        if output_dir is None: raise RhombusCLIProblem("Missing parameter: --out")
+        if id is None: raise RhombusCLIProblem("Missing parameter: --id")
+        
+        print(f"  Source: {source} :: {symbol}")
+        print(f"  Output: {output_dir.resolve()}")
+        
+        output_dir.mkdir(parents=True, exist_ok=True)
         
         with beet.DataPack(path=output_dir) as dp:
             
@@ -56,18 +76,11 @@ def compile_project(source: Path, symbol: str = None, output_dir: Path = None, i
                 raise TypeError
             
             target.inject(dp, id)
+            
+    else:
+        raise RhombusCLIProblem(f"Nothing to compile at path {source}")
     
-    # print(f"  Source: {source} :: {symbol}")
-    # print(f"  Output: {output_dir}")
-    # print(f"  ID:     {id}")
-    
-    output_dir.mkdir(parents=True, exist_ok=True)
-
-    print(f"Compiling {source} -> {output_dir}")
-    
-    
-        
-    print("Done")
+    print("\n[#5137d4]── Done " + "─" * round(0.6 * shutil.get_terminal_size().columns - 10))
 
 
 def rhombus_parser() -> argparse.ArgumentParser:
@@ -92,8 +105,10 @@ def main() -> None:
         if args.command == "compile":
             compile_project(source=args.source, symbol=args.symbol, output_dir=args.out, id=args.id)
         
-    except CLIError as e:
-        print("  " + e)
+    except RhombusCLIProblem as e:
+        print("")
+        print(f"  [#62a8f0]Error")
+        print(f"  [#62a8f0]╰─×[/#62a8f0] {str(e)}")
     
     except Exception as e:
         tb = e.__traceback__
