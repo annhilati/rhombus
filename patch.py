741e8c236aafbc8be37ea72b2a3d38557e725650
Annhilati <antbe.artis@web.de>
2026-09-19 18:59:23 +0200

    Oops

--- patch.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('rhombus/support/moredfs/types.py', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('), versions=("2.2.0", ...)):', ', versions=("2.2.0", ...)):')
-
-with open('rhombus/support/moredfs/types.py', 'w', encoding='utf-8') as f:
-    f.write(text)
